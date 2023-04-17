1b63b28db1354d638214fd0e5bf6f1edd164682f
david hernandez <davidfelipehernandez1@gmail.com>
2023-04-16 23:05:10 -0500

    update db

--- db/store.sql
+++ db/store.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 28, 2023 at 04:41 AM
+-- Generation Time: Apr 17, 2023 at 06:03 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -43,8 +43,8 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`P_ID`, `P_NAME`, `P_PRICE`, `P_DISCOUNT`, `P_AVAILABILITY`, `P_URL`, `P_DESCRIPTION`, `P_CATEGORY`) VALUES
-(1, 'terreneitor', 3333330, 0, 1, 'https://images3.memedroid.com/images/UPLOADED625/63c5fbc67a7d9.webp', 'terreneitor la maquina mas veloz de toda italia', 'toy'),
-(2, 'iPhone 13', 5680000, 0, 1, 'https://cdn.shopify.com/s/files/1/0604/8373/1606/products/IMG-6206240_600x.jpg?v=1661520210', 'Compatibilidad de red: 2G/3G/4G/5G\r\nTipo y Versión de SW: iOS 16\r\nCapacidad memoria RAM: 4 GB\r\nCapacidad de memoria interna en GigaBytes (GB): 128, 256 y 512 GB', 'mobile'),
+(1, 'terreneitor', 3333330, 0, 1, '', 'terreneitor la maquina mas veloz de toda italia', 'toy'),
+(2, 'iPhone 13', 5680000, 10, 1, 'https://cdn.shopify.com/s/files/1/0604/8373/1606/products/IMG-6206240_600x.jpg?v=1661520210', 'Compatibilidad de red: 2G/3G/4G/5G\r\nTipo y Versión de SW: iOS 16\r\nCapacidad memoria RAM: 4 GB\r\nCapacidad de memoria interna en GigaBytes (GB): 128, 256 y 512 GB', 'mobile'),
 (3, 'AMD RYZEN 7 3700X', 1699000, 0, 1, 'https://www.pcware.com.co/wp-content/uploads/2020/10/ryzen1.webp', 'Creados para servir mayor poder a usuarios exigentes. Asegura tu triunfo con procesadores de alto desempeño. Mayor eficiencia con un performance de  memoria optimizado.', 'hardware'),
 (4, 'IdeaPad 5 Gen 7 (14\" AMD)', 2850000, 0, 1, 'https://www.notebookcheck.com/fileadmin/Notebooks/News/_nc3/lenovo_laptop_ideapad_5_pro_gen_7_gallery_8_9.jpg', 'Laptop elegante de 14\" con procesadores móviles AMD Ryzen™\r\nTarjeta gráfica AMD Radeon™ integrada para un alto rendimiento sin problemas\r\nChasis delgado totalmente metálico (opcional) para ofrecer portabilidad y durabilidad', 'computer'),
 (5, 'Samsung s23', 4500000, 0, 1, 'https://images.samsung.com/co/smartphones/galaxy-s23/buy/kv_group_PC_v2.jpg', 'Consigue todos los detalles\r\nCapture más detalles con una cámara gran angular de 50MP.3 La tecnología Detail Enhancer utiliza IA para mejorar la profundidad y definición para obtener fotos de una calidad sorprendentemente alta.', 'mobile'),
@@ -53,6 +53,28 @@ INSERT INTO `products` (`P_ID`, `P_NAME`, `P_PRICE`, `P_DISCOUNT`, `P_AVAILABILI
 (8, 'megafono', 250000, 0, 1, 'https://images.unsplash.com/photo-1517816428104-797678c7cf0c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80', 'aparato con forma de cono utilizado para amplificar sonidos.', 'electronic'),
 (9, 'Gafas 3d', 100000, 0, 1, 'https://http2.mlstatic.com/D_NQ_NP_966499-MCO31545311148_072019-O.webp', '-Visor de alta calidad, Alta definición Sensor de movimiento\r\n-Despierta tus sentidos siente el vació de una caída\r\n-Diseñado para personas que usan o no lentes\r\n-Lente graduable para mayor nitidez y cuidado de tus ojos', 'electronic'),
 (10, 'Xiaomi Mi Smart Band 7', 182000, 0, 1, 'https://http2.mlstatic.com/D_NQ_NP_740669-MLA50416735265_062022-O.webp', 'Monitor de saturación de oxígeno en sangre\r\nEl nivel de oxígeno en sangre es un indicador muy importante para determinar el bienestar general. Podrás controlar este dato desde tu muñeca y evaluar tu condición física mientras realices tus actividades diarias o en sesiones de entrenamiento intenso.\r\n', 'electronic');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `U_ID` int(11) NOT NULL,
+  `U_NAME` varchar(50) NOT NULL,
+  `U_EMAIL` varchar(60) NOT NULL,
+  `U_PASSWORD` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`U_ID`, `U_NAME`, `U_EMAIL`, `U_PASSWORD`) VALUES
+(1, 'david', 'david@gmail', '123'),
+(2, 'Felipe', 'felipe@gmailcom', '123'),
+(3, 'Frontend Developer', 'generic@gmail.com', '123');
 
 --
 -- Indexes for dumped tables
@@ -65,6 +87,12 @@ ALTER TABLE `products`
   ADD PRIMARY KEY (`P_ID`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`U_ID`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -73,6 +101,12 @@ ALTER TABLE `products`
 --
 ALTER TABLE `products`
   MODIFY `P_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `U_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
