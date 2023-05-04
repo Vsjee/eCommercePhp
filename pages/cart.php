783f04ed24b33ac0430effdb60cd7bcfb658bfae
david hernandez <davidfelipehernandez1@gmail.com>
2023-05-04 12:07:59 -0500

    update cart desing

--- pages/cart.php
+++ pages/cart.php
@@ -1,4 +1,7 @@
 <?php
+
+include_once '../php/config.php';
+
 session_start();
 
 if (isset($_POST["add_to_cart"])) {
@@ -7,10 +10,11 @@ if (isset($_POST["add_to_cart"])) {
     if (!in_array($_GET["id"], $item_array_id)) {
       $count = count($_SESSION["shopping_cart"]);
       $item_array = array(
-        'item_id'               =>     $_GET["id"],
-        'item_name'               =>     $_POST["hidden_name"],
-        'item_price'          =>     $_POST["hidden_price"],
-        'item_quantity'          =>     $_POST["quantity"]
+        'item_id'       => $_GET["id"],
+        'item_name'     => $_POST["hidden_name"],
+        'item_price'    => $_POST["hidden_price"],
+        'item_quantity' => $_POST["quantity"],
+        'item_img'      => $_POST["hidden_img"]
       );
       $_SESSION["shopping_cart"][$count] = $item_array;
     } else {
@@ -19,10 +23,11 @@ if (isset($_POST["add_to_cart"])) {
     }
   } else {
     $item_array = array(
-      'item_id'               =>     $_GET["id"],
-      'item_name'               =>     $_POST["hidden_name"],
-      'item_price'          =>     $_POST["hidden_price"],
-      'item_quantity'          =>     $_POST["quantity"]
+      'item_id'       => $_GET["id"],
+      'item_name'     => $_POST["hidden_name"],
+      'item_price'    => $_POST["hidden_price"],
+      'item_quantity' => $_POST["quantity"],
+      'item_img'      => $_POST["hidden_img"]
     );
     $_SESSION["shopping_cart"][0] = $item_array;
   }
@@ -50,6 +55,12 @@ if (isset($_GET["action"])) {
 
   <!-- bootstrap cdn -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <style>
+    .img {
+      object-fit: cover;
+      height: 150px;
+    }
+  </style>
 </head>
 
 <body>
@@ -102,9 +113,10 @@ if (isset($_GET["action"])) {
   <div class="table-responsive">
     <table class="table table-bordered">
       <tr>
-        <th width="40%">Item Name</th>
+        <th width="10%">product</th>
+        <th width="10%">Item Name</th>
         <th width="10%">Quantity</th>
-        <th width="20%">Price</th>
+        <th width="10%">Price</th>
         <th width="15%">Total</th>
         <th width="5%">Action</th>
       </tr>
@@ -112,9 +124,24 @@ if (isset($_GET["action"])) {
       if (!empty($_SESSION["shopping_cart"])) {
         $total = 0;
         foreach ($_SESSION["shopping_cart"] as $keys => $values) {
+          $id = encryptor('encrypt', $values['item_id']);
       ?>
           <tr>
-            <td><?php echo $values["item_name"]; ?></td>
+            <td>
+              <a href="./productInfo.php?id=<?php echo $id ?>">
+                <img src="<?php
+                          if ($values["item_img"]) {
+                            echo $values["item_img"];
+                          } else {
+                            echo "https://www.fml.com.mx/wp-content/uploads/2016/04/Race-Registration-Image-Not-Found.png";
+                          } ?>" alt="<?php echo $values["item_name"]; ?>" title="<?php echo $values["item_name"]; ?>" class="w-100 img">
+            </td>
+            </a>
+            <td>
+              <a href="./productInfo.php?id=<?php echo $id ?>">
+                <?php echo $values["item_name"]; ?>
+              </a>
+            </td>
             <td><?php echo $values["item_quantity"]; ?></td>
             <td>$ <?php echo $values["item_price"]; ?></td>
             <td>$ <?php echo number_format($values["item_quantity"] * $values["item_price"], 2); ?></td>
@@ -125,9 +152,9 @@ if (isset($_GET["action"])) {
         }
         ?>
         <tr>
-          <td colspan="3" align="right">Total</td>
-          <td align="right">$ <?php echo number_format($total, 2); ?></td>
           <td></td>
+          <td colspan="4" align="right">Total</td>
+          <td align="right">$ <?php echo number_format($total, 2); ?></td>
         </tr>
       <?php
       }
