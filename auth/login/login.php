72cb7ba1410483a2112188526527f59c7ab705c9
david hernandez <davidfelipehernandez1@gmail.com>
2023-05-04 10:32:19 -0500

    fix min _session condition

--- auth/login/login.php
+++ auth/login/login.php
@@ -41,7 +41,7 @@ session_start();
               Carrito
               <span>
                 <?php
-                if ($_SESSION["shopping_cart"]) {
+                if (!empty($_SESSION["shopping_cart"])) {
                   echo count($_SESSION["shopping_cart"]);
                 } else {
                   echo 0;
